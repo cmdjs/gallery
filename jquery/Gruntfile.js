84cc91a471b5d34868c6e47ed40e515fb7d0a0ec
Hsiaoming Yang <lepture@me.com>
2013-02-05 14:26:04 +0800

    use replace to wrap jquery

--- jquery/Gruntfile.js
+++ jquery/Gruntfile.js
@@ -1,49 +1,41 @@
 module.exports = function(grunt) {
   var pkg = grunt.file.readJSON('package.json');
 
+  function repl(code, filename) {
+    var id = pkg.root + '/' + pkg.name + '/' + pkg.version + '/' + filename;
+    code = code.replace(/&&\s*define\.amd\s*&&\s*define\.amd\.jQuery/, '');
+    code = code.replace(/define\(\s*"jquery/, 'define("' + id);
+    return code;
+  }
+
   grunt.initConfig({
     pkg: pkg,
 
     download: {
       options: {
-        dest: 'src',
-        header: 'define(function(require) {',
-        footer: [
-          'return $.noConflict(true);',
-          '});'
-        ].join('\n')
+        dest: 'dist',
       },
       src: {
+        options: {
+          transform: function(code) {
+            return repl(code, 'jquery-debug');
+          }
+        },
         url: 'http://code.jquery.com/jquery-<%= pkg.version %>.js',
         name: 'jquery-debug.js'
       },
       min: {
+        options: {
+          transform: function(code) {
+            return repl(code, 'jquery');
+          }
+        },
         url: 'http://code.jquery.com/jquery-<%= pkg.version %>.min.js',
         name: 'jquery.js'
       }
-    },
-
-    'spm-transport': {
-      options: {
-        pkg: pkg,
-        src: 'src',
-        dest: 'dist'
-      },
-      min: {
-        src: ['src/jquery.js'],
-        options: {
-          uglify: {}
-        }
-      },
-      src: ['src/jquery-debug.js']
-    },
-
-    'spm-clean': {
-      all: ['src']
     }
   });
 
-  grunt.loadTasks('../_tasks/grunt-spm-build/tasks');
   grunt.loadTasks('../_tasks/download/tasks');
-  grunt.registerTask('build', ['download', 'spm-transport', 'spm-clean']);
+  grunt.registerTask('build', ['download']);
 };
