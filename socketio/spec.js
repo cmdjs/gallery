fabb12c68aad973baaf689cd252b330a520b18c9
Hsiaoming Yang <lepture@me.com>
2013-02-26 19:50:42 +0800

    remove socketio

--- socketio/spec.js
+++
@@ -1,8 +0,0 @@
-define(function(require) {
-  var module = require('./dist/socketio');
-  describe('socketio', function() {
-    it('should has version', function() {
-      expect(module.version).to.be.a('string');
-    });
-  });
-});
