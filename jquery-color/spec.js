218359e98bf550e8d3f54d10e82327b4fdff302e
Hsiaoming Yang <lepture@me.com>
2013-03-04 21:30:10 +0800

    delete jquery-color. move to seajs/jquery/color

--- jquery-color/spec.js
+++
@@ -1,9 +0,0 @@
-define(function(require) {
-  require('./dist/jquery-color');
-  var module = require('$');
-  describe('jquery color', function() {
-    it('should has Color method', function() {
-      expect(module.Color).to.be.a('function');
-    });
-  });
-});
