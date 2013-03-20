98bfa0ae6e27669f3513699b284455deb0a52c22
Hsiaoming Yang <lepture@me.com>
2013-03-20 17:18:12 +0800

    add runtime version

--- handlebars/Gruntfile.js
+++ handlebars/Gruntfile.js
@@ -13,13 +13,17 @@ module.exports = function(grunt) {
       src: {
         url: 'https://raw.github.com/wycats/handlebars.js/<%= pkg.version2 %>/dist/handlebars.js',
         name: 'handlebars.js'
+      },
+      runtime: {
+        url: 'https://raw.github.com/wycats/handlebars.js/<%= pkg.version2 %>/dist/handlebars.runtime.js',
+        name: 'runtime.js'
       }
     }
   });
 
 
-  require('../node_modules/grunt-spm-build/').init(grunt, {pkg: pkg});
-  grunt.loadTasks('../node_modules/grunt-spm-build/tasks');
+  require('grunt-spm-build').initConfig(grunt, {pkg: pkg});
   grunt.loadTasks('../_tasks/download/tasks');
-  grunt.registerTask('build', ['download', 'spm-build']);
+  grunt.loadGlobalTasks('grunt-spm-build');
+  grunt.registerTask('build', ['spm-build']);
 };
