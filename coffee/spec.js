194958859cc7e116faef5932917c84157072ddae
Hsiaoming Yang <me@lepture.com>
2013-04-03 10:07:39 +0800

    remove coffee

--- coffee/spec.js
+++
@@ -1,8 +0,0 @@
-define(function(require) {
-  var module = require('./dist/coffee-script');
-  describe('coffee', function() {
-    it('should has VERSION', function() {
-      expect(module.VERSION).to.be.a('string');
-    });
-  });
-});
