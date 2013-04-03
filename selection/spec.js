bb64ae6c30a06b0cace1922c28387e9b38127b74
Hsiaoming Yang <me@lepture.com>
2013-04-03 10:02:13 +0800

    remove selection

--- selection/spec.js
+++
@@ -1,8 +0,0 @@
-define(function(require) {
-  var module = require('./dist/selection');
-  describe('selection', function() {
-    it('should has version', function() {
-      expect(module.version).to.be.a('string');
-    });
-  });
-});
