e39500fb1b6bc9e74e5e32c6579664bfbd7b2134
afc163 <afc163@gmail.com>
2013-10-24 15:40:53 +0800

    add normalize@2.1.3

---
+++ normalize/Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+  var pkg = grunt.file.readJSON('package.json');
+
+  grunt.initConfig({
+    pkg: pkg,
+
+    download: {
+      options: {
+        dest: 'src',
+      },
+      src: {
+        options: {
+          transform: function(code) {
+            return code;
+          }
+        },
+        url: 'https://raw.github.com/necolas/normalize.css/v<%= pkg.version %>/normalize.css',
+        name: 'normalize.css'
+      }
+    }
+  });
+
+  grunt.loadGlobalTasks('spm-build');
+  grunt.util._.merge(grunt.config.data, require('spm-build').config);
+
+  grunt.loadTasks('../_tasks/download/tasks');
+  grunt.registerTask('build', ['download', 'spm-build']);
+};
